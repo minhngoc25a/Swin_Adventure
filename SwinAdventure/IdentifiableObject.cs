eb506e1c96f2aab8c95f683c9847b268bec0678f
minhngoc25a <minhnguyenduc72@gmail.com>
2022-06-18 16:58:18 +0700

    Iteration 1

---
+++ SwinAdventure/IdentifiableObject.cs
@@ -0,0 +1,29 @@
+﻿namespace SwinAdventure;
+
+public class IdentifiableObject
+{
+    private List<string> _identifiers = new List<string>();
+
+    public IdentifiableObject(string[] idents)
+    {
+        foreach (var id in idents)
+        {
+            _identifiers.Add(id.ToLower());
+        }
+    }
+
+    public bool AreYou(string id)
+    {
+        return _identifiers.Contains(id.ToLower());
+    }
+
+    public string FirstId()
+    {
+        return _identifiers[0];
+    }
+
+    public void AddIdentifier(string id)
+    {
+        _identifiers.Add(id.ToLower());
+    }
+}
---
+++ UnitTests/IdentifiableObJectTest.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using SwinAdventure;
+
+namespace UnitTests;
+
+[TestFixture]
+public class IdentifiableObjectTest
+{
+    [Test]
+    public void TestAreYou()
+    {
+        var id = new IdentifiableObject(new[] { "fred", "bob" });
+        Assert.IsTrue(id.AreYou("fred"));
+    }
+
+    [Test]
+    public void TestNotAreYou()
+    {
+        var id = new IdentifiableObject(new[] { "fred", "bob" });
+        Assert.IsFalse(id.AreYou("wilma"));
+    }
+
+    [Test]
+    public void TestCaseSensitive()
+    {
+        var id = new IdentifiableObject(new[] { "fred", "bob" });
+        Assert.IsTrue(id.AreYou("fReD"));
+    }
+
+    [Test]
+    public void TestFirstId()
+    {
+        var id = new IdentifiableObject(new[] { "fred", "bob" });
+        id.AddIdentifier("wilma");
+        Assert.IsTrue(id.AreYou("fred"));
+    }
+
+    [Test]
+    public void TestAddId()
+    {
+        var id = new IdentifiableObject(new[] { "fred", "bob" });
+        id.AddIdentifier("wilma");
+        Assert.IsTrue(id.AreYou("wilma"));
+    }
+}
